46f1f1f5e42ac90253f8ff6d1bc5a18b632eaa0e
SarthakJaingit <jainsarthak101@gmail.com>
2022-02-24 18:59:30 -0800

    blog 4 navigation final

--- Code/Server/Intelligent_Navigation.py
+++ Code/Server/Intelligent_Navigation.py
@@ -2,23 +2,50 @@ from Motor import *
 from servo import *
 import time
 from RoverGroundControl import *
+from Ultrasonic import *
 
-
-def move_for_a_foot(motor: Motor):
+def move_for_a_foot(motor: Motor, curr_strike):
     PWM.setMotorModel(-1000,-1000,-1000,-1000) #Backward
-    time.sleep(0.8)
+    if not (curr_strike):
+        time.sleep(0.8)
+    else:
+        time.sleep(0.2)
     PWM.setMotorModel(0,0,0,0) # Stop the rover
     time.sleep(1)
 
+def turn_row(motor: Motor):
+    pass
+
+def continue_forward(left_data, right_data, ultrasonic, strike):
+    full_data = list()
+    full_data.extend(right_data)
+    full_data.extend(left_data)
+    if sum(full_data):
+        data=ultrasonic.get_distance()
+        if data < 34:
+            # Will hit or be too close to obstacle in next move_for_a_foot()
+            return False
+        strike = 0
+        return True, strike
+    else:
+        strike += 1
+        if strike > 2:
+            return False, strike
+        return True, strike
 
 
 
 if __name__ == "__main__":
     PWM=Motor()
     pwm=Servo()
+    ultrasonic=Ultrasonic()
     pitstop_angle = 40
+    take_step = True
+    strike = 0
 
-    for i in range(0, 3):
-        move_for_a_foot(PWM)
-        side_scan(pwm, pitstop_angle, 'l')
-        side_scan(pwm, pitstop_angle, 'r')
+    while take_step:
+        move_for_a_foot(PWM, strike)
+        left_data = side_scan(pwm, pitstop_angle, 'l')
+        right_data = side_scan(pwm, pitstop_angle, 'r')
+
+        take_step, strike = continue_forward(left_data, right_data, ultrasonic, strike)
--- Code/Server/RoverGroundControl.py
+++ Code/Server/RoverGroundControl.py
@@ -1,18 +1,17 @@
 from servo import *
 import time
-# pwm=Servo()
-# pwm.setServoPwm('1', 160)
-# pwm.setServoPwm('0', 0)
+from VisionAndInference import *
 
 servo_origin = 90
 check_constants = [10, 45, 120]
 
 def side_scan(pwm, pitstop_angle, direction):
+    cam_report = list()
     if direction == 'l':
         for period in range(servo_origin, 9, -1):
             pwm.setServoPwm('0',period)
             if period in check_constants and period!= 90:
-                upward_check(pwm, 2) # Can change checking_count here
+                cam_report.extend(upward_check(pwm, 2)) # Can change checking_count here
             else:
                 time.sleep(0.01)
         for period in range(0, servo_origin + 1, 1):
@@ -23,7 +22,7 @@ def side_scan(pwm, pitstop_angle, direction):
         for period in range(servo_origin, 121, 1):
             pwm.setServoPwm('0',period)
             if period in check_constants and period!= 90:
-                upward_check(pwm, 2) # Can change checking_count here
+                cam_report.extend(upward_check(pwm, 2)) # Can change checking_count here
             else:
                 time.sleep(0.01)
         for period in range(121, servo_origin-1, -1):
@@ -32,8 +31,11 @@ def side_scan(pwm, pitstop_angle, direction):
     else:
         raise ValueError("Wrong input for direction")
 
+    return cam_report
+
 
 def upward_check(pwm, checking_count):
+    check_result = list()
     assert checking_count > 0 and checking_count <=3
     maximum_upward_angle = 120
     turn_angle = (maximum_upward_angle - servo_origin) / checking_count
@@ -41,12 +43,14 @@ def upward_check(pwm, checking_count):
     for period in range(servo_origin, maximum_upward_angle + 1, 1):
         pwm.setServoPwm('1',period)
         if (period - servo_origin) % turn_angle == 0 and period != 90:
-            time.sleep(2)
+            check_result.append(run_model())
         else:
             time.sleep(0.01)
     for period in range(maximum_upward_angle + 1, servo_origin - 1, -1):
         pwm.setServoPwm('1',period)
         time.sleep(0.01)
+
+    return check_result
 
 
 
