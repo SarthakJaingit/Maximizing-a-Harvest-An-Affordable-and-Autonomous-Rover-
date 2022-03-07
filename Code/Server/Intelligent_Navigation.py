a28f8012771b707e2b73bf3ebd273ba814e65c82
SarthakJaingit <jainsarthak101@gmail.com>
2022-03-06 21:52:15 -0800

    turn row + nav

--- Code/Server/Intelligent_Navigation.py
+++ Code/Server/Intelligent_Navigation.py
@@ -13,8 +13,50 @@ def move_for_a_foot(motor: Motor, curr_strike):
     PWM.setMotorModel(0,0,0,0) # Stop the rover
     time.sleep(1)
 
-def turn_row(motor: Motor):
-    pass
+def turn_row(motor: Motor, servo: Servo, ultrasonic: Ultrasonic):
+
+    data = ultrasonic.get_distance()
+    servo.setServoPwm('1',servo_origin)
+    servo.setServoPwm('0', servo_origin)
+
+    # Initiate Turning sequence
+    # Will make turning slow to preserve wheels
+    for iter in range(20):
+        motor.setMotorModel(2000,2000,-1500,-1500)
+        time.sleep(0.1)
+        motor.setMotorModel(0,0,0,0)
+
+    # Move to 90 degree formation
+    for period in range(servo_origin, 9, -1):
+        servo.setServoPwm('1',period)
+        time.sleep(0.01)
+
+    for period in range(servo_origin, 171, 1):
+        servo.setServoPwm('0', period)
+        time.sleep(0.01)
+
+    # Finding next row
+    while data < 34:
+        motor.setMotorModel(-1000,-1000,-1000,-1000)
+        time.sleep(0.1)
+        motor.setMotorModel(0,0,0,0)
+        data = ultrasonic.get_distance()
+
+    motor.setMotorModel(-1000, -1000, -1000, -1000)
+    time.sleep(0.4)
+    motor.setMotorModel(0, 0, 0, 0)
+
+    for period in range(170, 89, -1):
+        servo.setServoPwm('0', period)
+        time.sleep(0.1)
+    for period in range(servo_origin, 9, -1):
+        servo.setServoPwm('1',period)
+        time.sleep(0.01)
+
+    for iter in range(20):
+        motor.setMotorModel(2000,2000,-1500,-1500)
+        time.sleep(0.1)
+        motor.setMotorModel(0,0,0,0)
 
 def continue_forward(left_data, right_data, ultrasonic, strike):
     full_data = list()
@@ -22,12 +64,13 @@ def continue_forward(left_data, right_data, ultrasonic, strike):
     full_data.extend(left_data)
     if sum(full_data):
         data=ultrasonic.get_distance()
-        if data < 34:
+        if data < 10:
             # Will hit or be too close to obstacle in next move_for_a_foot()
-            return False
+            return False, -1
         strike = 0
         return True, strike
     else:
+        print("Strike...")
         strike += 1
         if strike > 2:
             return False, strike
@@ -38,6 +81,7 @@ def continue_forward(left_data, right_data, ultrasonic, strike):
 if __name__ == "__main__":
     PWM=Motor()
     pwm=Servo()
+
     ultrasonic=Ultrasonic()
     pitstop_angle = 40
     take_step = True
@@ -48,4 +92,15 @@ if __name__ == "__main__":
         left_data = side_scan(pwm, pitstop_angle, 'l')
         right_data = side_scan(pwm, pitstop_angle, 'r')
 
+ # Not running through strike. 
         take_step, strike = continue_forward(left_data, right_data, ultrasonic, strike)
+        if strike == -1:
+            print("Detected Object...")
+            break
+
+    print("Finished Monitoring ...")
+        # if strike:
+        #     led.ledIndex(0x02,255,125,0)
+        #     led.ledIndex(0x04,255,255,0)
+        # else:
+        #     led.colorWipe(led.strip, Color(0,0,0))
--- Code/Server/VisionAndInference.py
+++ Code/Server/VisionAndInference.py
@@ -2,25 +2,60 @@ import cv2
 from object_detector import ObjectDetector
 from object_detector import ObjectDetectorOptions
 
+import utils
+
 def initialize_model():
-    options = ObjectDetectorOptions(
+    high_options = ObjectDetectorOptions(
         num_threads=4,
         score_threshold=0.5, # This is where I can choose confidence thresholds
         max_results=10,
         enable_edgetpu=False)
-    detector = ObjectDetector(model_path="models/latestModelCER.tflite" , options=options)
+    high_detector = ObjectDetector(model_path="models/latestModelCER.tflite" , options=high_options)
+    low_options = ObjectDetectorOptions(
+        num_threads=4,
+        score_threshold=0.2, # This is where I can choose confidence thresholds
+        max_results=10,
+        enable_edgetpu=False)
+    low_detector = ObjectDetector(model_path = "models/Feb_6th_Noise_resistant_2nd.tflite", options=low_options)
 
-    return detector
+    return high_detector, low_detector
 
 def run_model() -> bool:
 
-    detector = initialize_model()
+    high_detector, low_detector = initialize_model()
 
     cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
+    alpha, beta = 1.3, 20
+    """ https://docs.opencv.org/3.4/d3/dc1/tutorial_basic_linear_transform.html """
+    frame = cv2.convertScaleAbs(frame, alpha=alpha, beta=beta)
 
     frame = cv2.flip(frame, 1)
-    detections = detector.detect(frame)
+    area_of_frame = frame.shape[0] * frame.shape[1] # It should always be 1920
+
+    detections = list()
+    detections.extend(high_detector.detect(frame))
+    detections.extend(low_detector.detect(frame))
+
+    cleaned_detections = list()
+
+    for detection in detections:
+        xmin = detection.bounding_box.left
+        ymin = detection.bounding_box.top
+        xmax = detection.bounding_box.right
+        ymax = detection.bounding_box.bottom
+
+        area = (xmax - xmin) * (ymax - ymin)
+        box_percent = area / area_of_frame
+
+        if box_percent < 0.2:
+            cleaned_detections.append(detection)
+
+    image = utils.visualize(frame, cleaned_detections)
+    # cv2.imwrite('flask_images/im8.jpg', image)
+    cv2.imshow('object_detector', image)
+    cv2.waitKey()
+
 
     possible_fruits = ['Apples', 'Strawberry', 'Peaches', 'Tomato']
 
@@ -33,6 +68,8 @@ def run_model() -> bool:
             '''Can add additional checks to see if bad spot bbox coord is within any fruit etc.'''
             if det in possible_fruits:
                 return True
+    if len(detected_labels):
+        return True
 
     return False
 
